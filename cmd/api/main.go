e17fe60146e3b7b3a47068fe33cf1650bf2abc40
rohan-7-m <rohan.m.rohan7@gmail.com>
2024-11-17 10:29:28 +0530

    Added healthCheck handler and server

---
+++ cmd/api/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+// later will be build time generated.
+const version = "1.0.0"
+
+// configuratoin settings for applications.
+type config struct {
+	port int
+	env  string
+}
+
+// application hold dependency for http handlers, helpers and middlewares.
+type application struct {
+	config config
+	logger *log.Logger
+}
+
+func main() {
+
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "DEV", "Environment (DEV|STAG|PROD)")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	// Start the HTTP
+	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
+	err := srv.ListenAndServe()
+	logger.Fatal(err)
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rohan-7-m/greenlight-api
+
+go 1.21.6
